e931b11e1735106a15787cae7100a8ce359fac47
Yashwardhan Khanna <48684054+SHAY2407@users.noreply.github.com>
2023-07-02 22:53:56 +0530

    base dashboard implemented

--- AdminDashboard/server/index.js
+++ AdminDashboard/server/index.js
@@ -5,15 +5,39 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+
+// Rate Limiter
+import { rateLimiter } from "./middlewares/rateLimiter.js";
+
+// Routes imports
 import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
-/* CONFIGURATION */
+// Data imports
+/*
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
+*/
+
+// Configuration
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(rateLimiter);
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
@@ -21,13 +45,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-/* ROUTES */
+// Routes Setup
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 
-/* MONGOOSE SETUP */
+// Mongoose Setup
 const PORT = process.env.PORT || 9000;
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -36,5 +60,14 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* 
+        User.insertMany(dataUser);
+        Product.insertMany(dataProduct);
+        ProductStat.insertMany(dataProductStat);
+        Transaction.insertMany(dataTransaction);
+        OverallStat.insertMany(dataOverallStat);
+        AffiliateStat.insertMany(dataAffiliateStat);
+    */
   })
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(`${error} did not connect.`));
--- AdminDashboard/server/routes/client.js
+++ AdminDashboard/server/routes/client.js
@@ -1,5 +1,18 @@
 import express from "express";
 
+import {
+  getProducts,
+  getCustomers,
+  getTransactions,
+  getGeography,
+} from "../controllers/client.js";
+
 const router = express.Router();
+
+// Routes
+router.get("/products", getProducts);
+router.get("/customers", getCustomers);
+router.get("/transactions", getTransactions);
+router.get("/occupancy", getGeography);
 
 export default router;
