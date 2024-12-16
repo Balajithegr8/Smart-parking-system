88c432e6d10c4f0c25a5dceba6c35c88b0693529
Balajithegr8 <br8440@srmist.edu.in>
2024-12-16 22:54:07 +0530

    added weather icon and href

--- MobileApp/src/scenes/Profile/Profile.jsx
+++ MobileApp/src/scenes/Profile/Profile.jsx
@@ -1,15 +1,19 @@
-import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import CloudIcon from "@mui/icons-material/Cloud";
 import React from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { useGetmobuserQuery } from "../../state/api";
 import { useGetmoblocQuery } from "../../state/api";
-import { styled } from '@mui/material/styles';
-import {Box,Grid,Paper}from '@mui/material';
+import { styled } from "@mui/material/styles";
+import { Box, Grid, Paper, Tooltip } from "@mui/material";
 
 const Profile = () => {
   const email = localStorage.getItem("email");
   const { data, isLoading, error } = useGetmobuserQuery(email);
-  const {data : data1,isLoading:isalsoloading,error:error1}=useGetmoblocQuery(email);
+  const {
+    data: data1,
+    isLoading: isalsoloading,
+    error: error1,
+  } = useGetmoblocQuery(email);
   if (isLoading || isalsoloading) {
     return <div>Loading...</div>;
   }
@@ -17,31 +21,31 @@ const Profile = () => {
   if (error || error1) {
     return <div>Error fetching data!</div>;
   }
-  
-  var licence_no="No vehicles registered";
-  const {name,occupation}=data;
-  if(data1){licence_no=data1.licence_no;}
-  
-  var parked="True";
-  if(licence_no==null || licence_no=="No vehicles registered"){
-    parked="False";
+
+  var licence_no = "No vehicles registered";
+  const { name, occupation } = data;
+  if (data1) {
+    licence_no = data1.licence_no;
   }
-  const useremail = email.split('@')[0];
+
+  var parked = "True";
+  if (licence_no == null || licence_no == "No vehicles registered") {
+    parked = "False";
+  }
+  const useremail = email.split("@")[0];
 
   const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(1),
-    color: '#FFFFFF', // Set text color to white
-    backgroundColor: 'transparent', // Make background transparent
-    boxShadow: 'none', // Remove shadow
-    border: 'none', // Remove border
-    backgroundImage: 'none', // Remove any default overlay
-    marginLeft: '1rem',
-    textAlign: 'center',
-    fontSize: '0.9rem',
+    color: "#FFFFFF", // Set text color to white
+    backgroundColor: "transparent", // Make background transparent
+    boxShadow: "none", // Remove shadow
+    border: "none", // Remove border
+    backgroundImage: "none", // Remove any default overlay
+    marginLeft: "1rem",
+    textAlign: "center",
+    fontSize: "0.9rem",
   }));
-  
-  
 
   return (
     <div
@@ -83,15 +87,24 @@ const Profile = () => {
       </div>
 
       <div>
-        <NotificationsNoneOutlinedIcon
-          style={{
-            color: "white",
-            position: "absolute",
-            right: "25px",
-            top: "40px",
-            fontSize: "3rem",
-          }}
-        />
+        <Tooltip title="Check Weather in your Area" arrow>
+          <a
+            href="https://weather-app-reactjs-nvep.onrender.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <CloudIcon
+              style={{
+                color: "white",
+                position: "absolute",
+                right: "25px",
+                top: "40px",
+                fontSize: "3rem",
+                zIndex: 9999,
+              }}
+            />
+          </a>
+        </Tooltip>
       </div>
 
       {/* Profile Card */}
@@ -114,13 +127,10 @@ const Profile = () => {
             width: "94%",
             textAlign: "center",
             alignContent: "center",
-            
           }}
         >
           {/* Avatar Section */}
-          <h2 style={{ fontSize: "1.7rem", fontWeight: "lighter" }}>
-            {name}
-          </h2>
+          <h2 style={{ fontSize: "1.7rem", fontWeight: "lighter" }}>{name}</h2>
           <div>
             <AccountCircleIcon style={{ fontSize: "6rem" }} />
           </div>
@@ -154,40 +164,44 @@ const Profile = () => {
             <span style={{ fontSize: "1rem", fontWeight: "light" }}>{parked}</span>
           </div> */}
 
-      <Box sx={{ width: '100%' }}>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}  >
-          <Grid item xs={6}>
-            <Item>Name</Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item>{name}</Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item>Email</Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item>{useremail}</Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item>Occupation</Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item>{occupation}</Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item>Vehicles</Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item>{licence_no}</Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item>Parked</Item>
-          </Grid>
-          <Grid item xs={6}>
-            <Item>{parked}</Item>
-          </Grid>
-        </Grid>
-    </Box>
+          <Box sx={{ width: "100%" }}>
+            <Grid
+              container
+              rowSpacing={1}
+              columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            >
+              <Grid item xs={6}>
+                <Item>Name</Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item>{name}</Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item>Email</Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item>{useremail}</Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item>Occupation</Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item>{occupation}</Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item>Vehicles</Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item>{licence_no}</Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item>Parked</Item>
+              </Grid>
+              <Grid item xs={6}>
+                <Item>{parked}</Item>
+              </Grid>
+            </Grid>
+          </Box>
 
           {/* Action Buttons */}
           <div style={{ marginTop: "1rem" }}>
